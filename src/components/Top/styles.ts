cd6dadca4526af7a0a0581fdae0b7af6f97d6a14
marvelopers <marvelopers@gmail.com>
2022-06-11 22:11:03 +0900

    delete

--- src/components/Top/styles.ts
+++
@@ -1,9 +0,0 @@
-import { css } from '@emotion/react';
-import colors from 'constants/colors';
-
-export const topStyle = css`
-  white-space: pre-line;
-  font-weight: bold;
-  color: ${colors.grey900};
-  padding: 0 24px;
-`;
